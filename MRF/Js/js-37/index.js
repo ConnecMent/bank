90856a48e75caecdb59727158f1a9730ee55f970
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-14 11:38:54 +0330

    refactor factory function using fetch :open_book:

--- MRF/Js/js-37/index.js
+++ MRF/Js/js-37/index.js
@@ -1,42 +1,103 @@
 //using factory function
-function callAPI({ baseURL }) {
+function callAPI({ baseURL, endPoint }) {
   return {
-    baseURL: baseURL,
-    get: function () {
-      return "get method called";
+    get: async function () {
+      return await fetch(`${baseURL}/${endPoint}`, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((val) => {
+        return val.json();
+      });
     },
 
-    post: function () {
-      return "post method called";
+    post: async function (body) {
+      return await fetch(`${baseURL}/${endPoint}`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((val) => {
+        return val.json();
+      });
     },
   };
 }
 
-const a = callAPI({ baseURL: "https://api.com" });
+async function testFactory() {
+  const a = callAPI({
+    baseURL: "https://jsonplaceholder.typicode.com",
+    endPoint: "posts",
+  });
+  const get = await a.get();
+  const post = await a.post({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  });
 
-console.log(a);
-console.log(a.baseURL);
-console.log(a.get());
-console.log(a.post());
+  console.log("=================");
+  console.log(">>> test factory function");
+  console.log(a);
+  console.log(">>> get method");
+  console.log(get);
+  console.log(">>> post method");
+  console.log(post);
+  console.log("=================");
+}
 
 // using class
 class callAPIclass {
-  constructor(baseURL) {
+  constructor(baseURL, endPoint) {
     this.baseURL = baseURL;
+    this.endPoint = endPoint;
   }
 
-  get() {
-    return "get method called";
+  async get() {
+    return await fetch(`${this.baseURL}/${this.endPoint}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((val) => {
+      return val.json();
+    });
   }
 
-  post() {
-    return "get method called";
+  async post(body) {
+    return await fetch(`${this.baseURL}/${this.endPoint}`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((val) => {
+      return val.json();
+    });
   }
 }
 
-const b = new callAPIclass("https://api2.com");
+async function testClass() {
+  const b = new callAPIclass("https://jsonplaceholder.typicode.com", "posts");
+  const get = await b.get();
+  const post = await b.post({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  });
 
-console.log(b);
-console.log(b.baseURL);
-console.log(b.get());
-console.log(b.post());
+  console.log("=================");
+  console.log(">>> test class");
+  console.log(b);
+  console.log(">>> get method");
+  console.log(get);
+  console.log(">>> post method");
+  console.log(post);
+  console.log("=================");
+}
+
+// run test scripts
+testFactory();
+testClass();
