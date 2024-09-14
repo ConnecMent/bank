8411512677ebfe6da169253a9af778d0c36b3b95
ramin <ramin.lgh.82@gmail.com>
2024-09-14 10:00:00 +0330

    change pino logging to work automatically

--- ramin/logService.js
+++ ramin/logService.js
@@ -1,55 +1,34 @@
 const fs = require('fs-extra');
 const path = require('path');
 const pino = require('pino');
-const { setInterval } = require('timers');
+const pinoRoll = require('pino-roll');
 
 const logDir = path.join(__dirname, 'logs');
-const currentHour = new Date().getHours();
 
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
-const streams = [
-  { stream: pino.transport({
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-      },
-    }), },
-  { stream: pino.destination({
-      dest: path.join(logDir, `log-${new Date().toISOString().slice(0, 13).replace(/:/g, '-')}.log`),
-      sync: false
-    }) 
+const rollStream = pinoRoll({
+  dest: path.join(logDir, 'log-%DATE%.log'),
+  frequency: '1h',
+  date_format: 'YYYY-MM-DD-HH',
+  maxAge: '2h',
+});
+
+const logger = pino({
+  level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+    },
   },
-];
+}, rollStream);
 
-const logger = pino({ level: 'info' }, pino.multistream(streams));
-
-
-setInterval(() => {
-  const now = new Date();
-  
-  const timestamp = now.toISOString().slice(0, 13).replace(/:/g, '-');
-  const newLogFile = path.join(logDir, `log-${timestamp}.log`);
-  
-  if (newLogFile !== currentHour) {
-    logger.info(`Changing log file to: ${newLogFile}`);
-    logger.streams[1].stream = pino.destination({ dest: newLogFile, sync: false });
-  }
-  
-  const twoHoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000).getTime();
-  fs.readdirSync(logDir).forEach(file => {
-    const filePath = path.join(logDir, file);
-    if (fs.statSync(filePath).mtime.getTime() < twoHoursAgo) {
-      fs.unlinkSync(filePath);
-      logger.info(`Deleted log file: ${filePath}`);
-    }
-  });
-}, 60 * 60 * 1000); 
 setInterval(() => {
   const logLevels = ['fatal', 'error', 'warn', 'info', 'debug', 'trace'];
   const randomLevel = logLevels[Math.floor(Math.random() * logLevels.length)];
   logger[randomLevel](`Logging at level: ${randomLevel}`);
-}, 10 * 1000); 
+}, 10 * 1000);
